699c3b11098909882c24f76eccdce6a9c3b1f4ef
hagnoykmik <ymj02148@naver.com>
2022-08-16 02:42:03 +0900

    전기버스-ing

--- 02_List1/05_전기버스/s1.py
+++ 02_List1/05_전기버스/s1.py
@@ -18,11 +18,21 @@ for tc in range(1, t + 1):
 
     cnt = 0 #충전 횟수
     s = 0 #start위치는 idx = 0
-    while s + k < n:
-        for j in range(s, s + k + 1): #1번 충전으로 갈 수 있는 거리 범위 내에서
-            if arr[j] == 1:
-                cnt += 1
-                s = j
+
+    while s < n:
+        for j in range(k): #충전으로 갈 수 있는 거리 범위 내에서
+            if s + (k - j)  == 1: #충전소가 있으면
+                s = k - i
+                cnt += 1 #충전한다
+                s = j #start위치를 그 위치로 바꿔준다
+                break
+
+        else: #이동가능한 거리에 충전소가 없으면 0 반환
+            cnt = 0
+            break
+
+    print(f'#{tc} {cnt}')
+
 
 
 
