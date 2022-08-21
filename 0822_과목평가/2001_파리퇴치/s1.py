a8ef90011cb29e09f07f0b9454935b9b758c5923
hagnoykmik <ymj02148@naver.com>
2022-08-22 00:58:54 +0900

    파리퇴치_완

--- 0822_과목평가/2001_파리퇴치/s1.py
+++ 0822_과목평가/2001_파리퇴치/s1.py
@@ -5,21 +5,22 @@ t = int(input())
 
 for tc in range(1, t + 1):
     n, m = map(int, input().split())
-
     board = [list(map(int, input().split())) for _ in range(n)]
+    max_s = 0  # 합의 최대값
 
-    # m x m 범위를 돌면서 fly의 값을 더한다
-    cnt = 0  # 행이 바뀔 때 초기화
-    max_c = 0
+    # n x n 배열 탐색
+    for i in range(n - m + 1):
+        for j in range(n - m + 1):
 
-    for r in range(n):
-        for c in range(n):
-            for i in range(n-m+1):
-                for j in range(n-m+1):
-                    cnt += board[i][j]
+            sum_f = 0  # 파리의 합
 
-    # 최대값을 구한다
-    if max_c < cnt:
-        max_c = cnt
+            # m x m 배열 탐색
+            for k in range(i, i + m):
+                for l in range(j, j + m):
+                    sum_f += board[k][l]
 
-    print(f'#{tc} {max_c}')
+            # 최대값을 구한다
+            if max_s < sum_f:
+                max_s = sum_f
+
+    print(f'#{tc} {max_s}')
